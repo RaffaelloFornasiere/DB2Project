4377313ce10d93ff720ac8c2d51225fbe427129e
raffaellofornasiere <fornasiere.raffaello@mail.polimi.it>
2021-11-03 08:14:26 +0100

    initial commit

---
+++ src/test/java/it/polimi/db2/telecoApp/TelecoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.db2.telecoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
