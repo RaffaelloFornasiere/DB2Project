bb8fbf3f5e66d90173230c19beaee7b81b41f7fe
RaffaelloFornasiere <fornasiere.raffaello@gmail.com>
2021-11-16 17:52:13 +0100

    commit

--- DB2Project-fe/src/app/app-routing.module.ts
+++ DB2Project-fe/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {PackagesComponent} from "./pages/packages/packages.component";
 import {SettingsComponent} from "./pages/settings/settings.component";
+import {UserSettingsComponent} from "./pages/user-settings/user-settings.component";
+import {AdminSettingsComponent} from "./pages/admin-settings/admin-settings.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 
 const routes: Routes = [
@@ -10,6 +14,10 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'packages', component: PackagesComponent},
   { path: 'settings', component: SettingsComponent},
+  {path: 'user', component: UserSettingsComponent},
+  {path: 'admin', component: AdminSettingsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ]
 
 @NgModule({
--- DB2Project-fe/src/app/services/app.service.ts
+++ DB2Project-fe/src/app/services/app.service.ts
@@ -18,11 +18,17 @@ export class AppService {
       authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
     } : {});
 
-    this.http.get('user', {headers: headers}).subscribe(response => {
-      // @ts-ignore
-      this.authenticated = !!response['name'];
-      return callback && callback();
-    });
+    this.http.get('api/user', {headers: headers})
+      .subscribe(
+        response => {
+          // @ts-ignore
+          this.authenticated = !!response['name'];
+          return callback && callback();
+        },
+        error =>{
+          console.log(error)
+
+        });
 
   }
 }
